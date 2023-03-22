1d22e9add2582ba73530f1cde61076bb98186dc9
Hassan Raza <hassan.raza@walmart.com>
2023-03-23 01:21:58 +0530

    Add computer canvas with PC model loaded

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import {
   Contact,
   Experience,
   Feedbacks,
-  Hero,
   Navbar,
   Tech,
   Works,
   StarsCanvas,
+  SystemModel,
 } from "./components";
 
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
-        <Hero />
+        <SystemModel />
       </div>
       <About />
       <Experience />
